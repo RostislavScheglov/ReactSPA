7e3ea44cc529250e39b6263bbdb8eff3ac3c9675
RostislavScheglov <55809532+RostislavScheglov@users.noreply.github.com>
2020-11-10 01:54:48 +0300

    Updates

---
+++ precache-manifest.42264712c530f717ac3bd0d9939f21c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75a85812c77bb027fb2b36aabab47e5",
+    "url": "/ReactSPA/index.html"
+  },
+  {
+    "revision": "ff2ac0daa148b33d67a2",
+    "url": "/ReactSPA/static/css/main.fc67a3ce.chunk.css"
+  },
+  {
+    "revision": "7cda945de2126823d20f",
+    "url": "/ReactSPA/static/js/2.beb18d46.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ReactSPA/static/js/2.beb18d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff2ac0daa148b33d67a2",
+    "url": "/ReactSPA/static/js/main.7df38eb6.chunk.js"
+  },
+  {
+    "revision": "306d214e71821ad66558",
+    "url": "/ReactSPA/static/js/runtime-main.b7438632.js"
+  },
+  {
+    "revision": "ec9b8fb60a6c1b38cc44fdc73aea1256",
+    "url": "/ReactSPA/static/media/bg_left.ec9b8fb6.svg"
+  },
+  {
+    "revision": "9a9b39653441a6ee3e42c2da76fdf24f",
+    "url": "/ReactSPA/static/media/button_add.9a9b3965.svg"
+  },
+  {
+    "revision": "2f9fba3717b3f061b736d89a920db198",
+    "url": "/ReactSPA/static/media/button_delet.2f9fba37.svg"
+  },
+  {
+    "revision": "b6da1acd27e2055c08bf00dff2a63996",
+    "url": "/ReactSPA/static/media/checkbox_done.b6da1acd.svg"
+  },
+  {
+    "revision": "e576cda6ea3226fb36da25bb994f5cb4",
+    "url": "/ReactSPA/static/media/logo.e576cda6.svg"
+  }
+]);
